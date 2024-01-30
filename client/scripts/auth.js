db64bd3eebd35d930df7b1cca012e6ce6628bbe2
henrychris <henrychris2003@gmail.com>
2024-01-30 12:35:41 +0100

    Create auth.js

---
+++ client/scripts/auth.js
@@ -0,0 +1,25 @@
+import { postData } from "./helper.js";
+import { BASE_URL } from "./config.js";
+
+export async function login(email, password) {
+  const res = await postData(`${BASE_URL}/auth/login`, {
+    emailAddress: email,
+    password: password,
+  });
+
+  return res.data;
+}
+
+export async function register(firstName, lastName, email, password) {
+  const role = "User";
+
+  const res = await postData(`${BASE_URL}/auth/register`, {
+    firstName: firstName,
+    lastName: lastName,
+    emailAddress: email,
+    password: password,
+    role: role,
+  });
+
+  return res.data;
+}
