988acf24502e48b0b645d004e58dd5c2971ed92d
henrychris <henrychris2003@gmail.com>
2024-01-31 14:01:28 +0100

    add strong typing to signalR methods

--- src/AuctionApp.Application/Contracts/SignalRClients/IAuctionRoomClient.cs
+++ src/AuctionApp.Application/Contracts/SignalRClients/IAuctionRoomClient.cs
@@ -1,8 +1,19 @@
+using AuctionApp.Domain.Entities.Hub;
+
 namespace AuctionApp.Application.Contracts.SignalRClients;
 
 public interface IAuctionRoomClient
 {
     Task SendMessageToRoom(string roomName, string content);
-    
-    Task OnDisconnectedAsync(Exception? exception);
+    Task JoinRoom(string connectionId, string roomId);
+    Task LeaveRoom(string connectionId, string roomId);
+    Task CloseGroup(string roomId);
+
+    Task UserLeft(string username);
+    Task UserJoined(string username);
+    Task KickUser(GroupUser? user);
+    Task ReceiveMessage(Message message);
+    Task AuctionStarted(string message);
+    Task BidPlaced(Message message, decimal amountInNaira);
+    Task AuctionEnded(string winnerName, decimal priceInNaira);
 }
